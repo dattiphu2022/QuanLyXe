3aeac3ffddfe80d5ef91f6dc72296b72e8620ea6
Game Master <friendlyhospital@gmail.com>
2022-11-17 21:13:50 +0700

    update MotoBase

--- QuanLyXe/Domain/BaseClasses/MotoBase.cs
+++ QuanLyXe/Domain/BaseClasses/MotoBase.cs
@@ -2,5 +2,19 @@
 
 abstract class MotoBase : IVehicle
 {
-    
+    protected MotoBase(
+        string name, 
+        int serialNumber, 
+        Color color)
+    {
+        VehicleName = name;
+        SerialNumber = serialNumber;
+        DoorCount = 0;
+        Color = color;
+    }
+
+    public string VehicleName { get; set; } = null!;
+    public int SerialNumber { get; set; }
+    public int DoorCount { get; set; }
+    public Color Color { get; set; }
 }
