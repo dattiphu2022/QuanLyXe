cb5573006003110411cb407a31f147fec3068b34
Game Master <friendlyhospital@gmail.com>
2022-11-17 21:12:41 +0700

    udpate CarBase

--- QuanLyXe/Domain/BaseClasses/CarBase.cs
+++ QuanLyXe/Domain/BaseClasses/CarBase.cs
@@ -2,5 +2,20 @@
 
 abstract class CarBase : IVehicle
 {
-    
+    protected CarBase(
+        string name, 
+        int serialNumber, 
+        int doorCount, 
+        Color color)
+    {
+        VehicleName = name;
+        SerialNumber = serialNumber;
+        DoorCount = doorCount;
+        Color = color;
+    }
+
+    public string VehicleName { get; set; } = null!;
+    public int SerialNumber { get; set; }
+    public int DoorCount { get; set; }
+    public Color Color { get; set; }
 }
