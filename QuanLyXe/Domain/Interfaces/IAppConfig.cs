c1730ebaa0acda1f0d7af309fecc100ed2adab24
Game Master <friendlyhospital@gmail.com>
2022-11-25 15:52:59 +0700

    update IAppConfig

--- QuanLyXe/Domain/Interfaces/IAppConfig.cs
+++ QuanLyXe/Domain/Interfaces/IAppConfig.cs
@@ -4,6 +4,7 @@ namespace QuanLyXe.Domain.Interfaces
 {
     internal interface IAppConfig
     {
-
+        string DataFileName { get; }
+        JsonSerializerSettings JsonSerializerSettings { get; }
     }
 }
