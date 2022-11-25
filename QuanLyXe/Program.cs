90adcd963a92d38ab262438244552f64a60678cd
Game Master <friendlyhospital@gmail.com>
2022-11-25 16:32:08 +0700

    update Program: demo input and console output completed

--- QuanLyXe/Program.cs
+++ QuanLyXe/Program.cs
@@ -7,19 +7,35 @@ using QuanLyXe.Providers;
 internal class Program
 {
     private static void Main(string[] args)
-    {        
+    {
         IServiceProvider services = BuildServices();
 
         IDataReader reader = services.GetRequiredService<IDataReader>();
         IDataWriter writer = services.GetRequiredService<IDataWriter>();
         HalfDoorCountManipulator halfer = services.GetRequiredService<HalfDoorCountManipulator>();
+        ResetDefaultDoorCountManipulator reseter = services.GetRequiredService<ResetDefaultDoorCountManipulator>();
+        DoubleDoorCountManipulator doubler = services.GetRequiredService<DoubleDoorCountManipulator>();
 
         var vehicles = reader.GetAll();
 
+        if (reader is DemoDataReader)
+        {
+            writer.Write(datas: vehicles, message: "ORIGIN");
+        }
 
         halfer.Manipulate(vehicles);
         writer.Write(datas: vehicles, message: "DEVIDED BY 2 - FIRST");
         
+        halfer.Manipulate(vehicles);
+        writer.Write(datas: vehicles, message: "DEVIDED BY 2 - SECOND");
+
+        reseter.Manipulate(vehicles);
+        writer.Write(datas: vehicles, message: "RESETED");
+
+        doubler.Manipulate(vehicles);
+        writer.Write(datas: vehicles, message:"DOUBLED");
+
+        //Console.ReadLine();
     }
 
     private static IServiceProvider BuildServices()
@@ -37,6 +53,8 @@ internal class Program
         });
 
         service.AddScoped<HalfDoorCountManipulator>();
+        service.AddScoped<DoubleDoorCountManipulator>();
+        service.AddScoped<ResetDefaultDoorCountManipulator>();
         service.AddScoped<IDataReader, DemoDataReader>();
         service.AddScoped<IDataWriter, ConsoleWriter>();
 
