1374e2c5219956e3204680e3c227caf5c2d49c7f
Game Master <friendlyhospital@gmail.com>
2022-11-25 16:16:37 +0700

    update Program.cs: make file no code

---
+++ QuanLyXe/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using QuanLyXe.Data.Manipulators;
+using QuanLyXe.Domain.Interfaces;
+using QuanLyXe.Providers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {        
+    }
+}
