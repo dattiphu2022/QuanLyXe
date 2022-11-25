bb1cc2ade65a13e95ac9af6fc3b87d193538bff7
Game Master <friendlyhospital@gmail.com>
2022-11-25 16:14:20 +0700

    add ManipulatorBase

---
+++ QuanLyXe/Domain/BaseClasses/ManipulatorBase.cs
@@ -0,0 +1,11 @@
+﻿using QuanLyXe.Domain.Interfaces;
+
+namespace QuanLyXe.Domain.BaseClasses
+{
+    internal abstract class ManipulatorBase<T>
+        : IManipulator<T>
+    {
+        public abstract T Manipulate(T data);
+        public abstract IEnumerable<T> Manipulate(IEnumerable<T> data);
+    }
+}
