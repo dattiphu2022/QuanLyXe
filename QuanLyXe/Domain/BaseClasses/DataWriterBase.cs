ff6b720dd0905cdfe6fef4a75ded4a622b2e4ec1
Game Master <friendlyhospital@gmail.com>
2022-11-25 15:50:53 +0700

    update DataWriterBase

--- QuanLyXe/Domain/BaseClasses/DataWriterBase.cs
+++ QuanLyXe/Domain/BaseClasses/DataWriterBase.cs
@@ -4,6 +4,7 @@ namespace QuanLyXe.Domain.BaseClasses
 {
     internal abstract class DataWriterBase : IDataWriter
     {
-
+        public abstract bool Write(IEnumerable<IVehicle> datas);
+        public abstract void Write(IEnumerable<IVehicle> datas, string message);
     }
 }
