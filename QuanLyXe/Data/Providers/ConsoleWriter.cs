1c3ac68942cc62b6bc27ad306258d0747d7c8232
Game Master <friendlyhospital@gmail.com>
2022-11-25 16:13:14 +0700

    add ConsoleWriter

---
+++ QuanLyXe/Data/Providers/ConsoleWriter.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using QuanLyXe.Domain.BaseClasses;
+using QuanLyXe.Domain.Interfaces;
+
+namespace QuanLyXe.Providers
+{
+    internal class ConsoleWriter : DataWriterBase
+    {
+        public override bool Write(IEnumerable<IVehicle> datas)
+        {
+            try
+            {
+                foreach (IVehicle vehicle in datas)
+                {
+                    Console.WriteLine($"\t{vehicle.VehicleName}\t{vehicle.SerialNumber}\t{vehicle.DoorCount}\t{vehicle.Color.Name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO: log ex
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void Write(IEnumerable<IVehicle> datas, string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"<{message}>");
+
+            Write(datas);
+
+            Console.WriteLine($"</{message}>");
+
+        }
+
+    }
+}
