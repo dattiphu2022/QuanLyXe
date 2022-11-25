3d1c04f4d7f8928262c4ac6110b919c86b14dde5
Game Master <friendlyhospital@gmail.com>
2022-11-25 15:53:41 +0700

    update AppConfig

--- QuanLyXe/Domain/Models/System/AppConfig.cs
+++ QuanLyXe/Domain/Models/System/AppConfig.cs
@@ -3,5 +3,7 @@ using QuanLyXe.Domain.Interfaces;
 
 internal class AppConfig : IAppConfig
 {
+    public string DataFileName { get; set; } = null!;
 
+    public JsonSerializerSettings JsonSerializerSettings { get; set; } = null!;
 }
