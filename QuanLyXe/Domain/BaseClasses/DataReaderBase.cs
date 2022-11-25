15553fcad060be1d6b3be623e32560a6bcc5c89b
Game Master <friendlyhospital@gmail.com>
2022-11-25 15:41:54 +0700

    update DataReaderBase

--- QuanLyXe/Domain/BaseClasses/DataReaderBase.cs
+++ QuanLyXe/Domain/BaseClasses/DataReaderBase.cs
@@ -4,6 +4,10 @@ namespace QuanLyXe.Domain.BaseClasses
 {
     internal abstract class DataReaderBase : IDataReader
     {
-        
+        public abstract Task<IEnumerable<IVehicle>> GetAllAsync();
+        public IEnumerable<IVehicle> GetAll()
+        {
+            return GetAllAsync().GetAwaiter().GetResult();
+        }
     }
 }
