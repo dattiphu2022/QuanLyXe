1e74834093d0a3a6e80142751938dc59aa7a02b0
Game Master <friendlyhospital@gmail.com>
2022-11-25 16:12:59 +0700

    update LocaStorageWriter

--- QuanLyXe/Data/Providers/LocalStorageWriter.cs
+++ QuanLyXe/Data/Providers/LocalStorageWriter.cs
@@ -6,6 +6,31 @@ namespace QuanLyXe.Providers
 {
     internal class LocalStorageWriter : DataWriterBase
     {
+        private readonly IAppConfig config;
 
+        public LocalStorageWriter(IAppConfig config)
+        {
+            this.config = config;
+        }
+
+        public override bool Write(IEnumerable<IVehicle> datas)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(datas, config.JsonSerializerSettings);
+                File.WriteAllText(config.DataFileName, json);
+            }
+            catch (Exception)
+            {
+                //TODO: log ex
+                return false;
+            }
+            return true;
+        }
+
+        public override void Write(IEnumerable<IVehicle> datas, string message)
+        {
+            Write(datas);
+        }
     }
 }
