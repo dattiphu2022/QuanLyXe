6653ad51c63730f2ef0dcc51403aefab4a479c83
Game Master <friendlyhospital@gmail.com>
2022-11-26 21:56:20 +0700

    add global using

--- QuanLyXe.Tests/Usings.cs
+++ QuanLyXe.Tests/Usings.cs
@@ -1,1 +1,5 @@
 global using NUnit.Framework;
+global using QuanLyXe.Data.Manipulators;
+global using QuanLyXe.Providers;
+global using QuanLyXe.Domain.BaseClasses;
+global using QuanLyXe.Domain.Interfaces;
